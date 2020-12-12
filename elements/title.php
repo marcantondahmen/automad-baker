8d6218b44e9619b5007161ecbc1bfb6acdec6d57
Marc Anton Dahmen <mail@marcdahmen.de>
2020-12-12 11:16:16 +0100

    Update title.php

--- elements/title.php
+++ elements/title.php
@@ -4,7 +4,7 @@
 		<h1>@{ title }</h1>
 		<@ tags.php @>	
 		<@ if not @{ checkboxHideDate } and @{ date } @>
-			<div class="uk-margin-small-bottom">@{ date | dateFormat (@{ formatDate | def ('l, F jS Y')}, @{ locale }) }</div>
+			<div class="uk-margin-small-bottom uk-margin-small-top">@{ date | dateFormat (@{ formatDate | def ('l, F jS Y')}, @{ locale }) }</div>
 		<@ end @>
 	</div>
 <@ end @>
