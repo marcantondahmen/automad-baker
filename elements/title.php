14cce4e911a81ecee5fbd91063daae8aec3dfe52
Marc Anton Dahmen <mail@marcdahmen.de>
2021-04-24 17:01:15 +0200

    Minor fix

--- elements/title.php
+++ elements/title.php
@@ -1,6 +1,6 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 <@ if not @{ checkboxHideTitle } @>
-	<div class="am-block uk-margin-small-bottom">
+	<div class="am-block uk-margin-small-bottom uk-margin-top-remove">
 		<h1>@{ title }</h1>
 		<@ tags.php @>	
 		<@ if not @{ checkboxHideDate } and @{ date } @>
