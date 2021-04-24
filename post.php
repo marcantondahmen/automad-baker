9979b6d83e714b09f7af587be478beb460a8d403
Marc Anton Dahmen <mail@marcdahmen.de>
2021-04-24 22:01:16 +0200

    Minor fixes

--- post.php
+++ post.php
@@ -1,7 +1,7 @@
 <?php defined('AUTOMAD') or die('Direct access not permitted!'); ?>
 <@ elements/header.php @>
 
-		<ul class="uk-grid" data-baker-scrolled>
+		<ul class="uk-grid">
 			<li class="uk-width-1-1 baker-navbar-push">
 				<@ set { :gridSize: 'large' } @>
 				<@ if @{ checkboxSmallPagelistGrid } @>
